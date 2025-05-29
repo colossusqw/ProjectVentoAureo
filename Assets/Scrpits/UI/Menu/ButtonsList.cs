8290e550da89a071fc5aa551ff2f0b24ed71cab7
Danilo Silva dos Santos <Dsaotos4002@gmail.com>
2025-05-29 20:23:39 -0300

    Implementing the Main Menu and Options Menu

--- Assets/Scrpits/UI/Menu/ButtonsList.cs
+++ Assets/Scrpits/UI/Menu/ButtonsList.cs
@@ -1,20 +1,44 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ButtonsList : MonoBehaviour
 {
+    [SerializeField] GameObject mainMenu;
+    [SerializeField] GameObject optionsMenu;
+    [SerializeField] private Slider volumeSlider;
     void Awake()
     {
         Cursor.lockState = CursorLockMode.Confined;
     }
 
+    private void Start()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("volume", 0.8f);
+        volumeSlider.onValueChanged.AddListener(SetVolume);
+
+        mainMenu.SetActive(true);
+        optionsMenu.SetActive(false);
+    }
+
     public void LeaveGame()
     {
         Application.Quit();
+
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
     }
 
     public void StartGame()
     {
-        GameManager.GM.ChangeScene("Level");
+        GameManager.GM.ChangeScene("Rules");
+    }
+    
+    public void SetVolume(float volume)
+    {
+        PlayerPrefs.SetFloat("volume", volume);
+        PlayerPrefs.Save();
+        AudioListener.volume = volume;
     }
 }
